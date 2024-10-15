ce72be922c7b0015ebf6698393b01d288f39c0d0
David DeRienzo <dderienzo645@hotmail.com>
2024-10-15 16:32:45 -0400

    Writing alternate installation using pyproject.toml.

--- setup.py
+++ setup.py
@@ -13,19 +13,4 @@ else:
     raise RuntimeError("Unable to find version string in %s." % (version_file,))
 print('FASTPT version is %s'%(fastpt_version))
 
-setup(
-    name='fast-pt',
-    description=(
-        "FAST-PT is a code to calculate quantities in cosmological "
-        "perturbation theory at 1-loop (including, e.g., corrections to the "
-        "matter power spectrum)."),
-    author="Joseph E. McEwen, Xiao Fang, and Jonathan Blazek (blazek@berkeley.edu)",
-    author_email="blazek@berkeley.edu",
-    packages=find_packages(),
-    install_requires=['numpy', 'scipy', 'matplotlib'],
-    version=fastpt_version,
-    license="MIT license",
-    url="https://github.com/JoeMcEwen/FAST-PT",
-    download_url="https://github.com/JoeMcEwen/FAST-PT/archive/v%s.zip"%fastpt_version,
-    
-)
+setup(version=fastpt_version)
