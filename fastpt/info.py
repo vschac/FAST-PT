5a47a226e2648c510158cce822d5ff7f17fd29ab
David DeRienzo <dderienzo645@hotmail.com>
2024-10-22 15:43:27 -0400

    Adding commit hash to version.

--- fastpt/info.py
+++ fastpt/info.py
@@ -2,4 +2,4 @@
 Place package information here. Because it is not a local package (e.g. __init__.py),
 the information is available whether the package is loaded from outside or inside the directory.
 '''
-__version__ = '3.2'
+__version__ = '3.2+git.35005b9ef80c243b6ec307195646af734c831829'
