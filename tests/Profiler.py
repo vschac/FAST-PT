768ccbfdeb950561c9586341e6eef7cf263f5489
Vincent Schacknies <vincentschacknies@Vincents-MacBook-Pro.local>
2025-02-12 11:41:25 -0500

    profiling for long functions

--- tests/Profiler.py
+++ tests/Profiler.py
@@ -2,7 +2,7 @@ from fastpt import FASTPT
 import numpy as np
 import os
 from line_profiler import LineProfiler
-from fastpt import initialize_params
+from fastpt import IA_ct
 
 def profile_fastpt():
     data_path = os.path.join(os.path.dirname(__file__), '..', 'examples', 'Pk_test.dat')
@@ -10,12 +10,12 @@ def profile_fastpt():
     k = d[:, 0]
     
     n_pad = int(0.5 * len(k))
-    fpt = FASTPT(k, to_do=['one_loop_dd'], low_extrap=-5, high_extrap=3, n_pad=n_pad)
+    fpt = FASTPT(k, to_do=['tij'], low_extrap=-5, high_extrap=3, n_pad=n_pad)
 
 
 # Profile specific methods
 lprofiler = LineProfiler()
-lprofiler.add_function(FASTPT.__init__)
+lprofiler.add_module(IA_ct)
 '''
 Longer runtime functions:
 - initialize_params.g_m_vals (lines 38 and 42)
